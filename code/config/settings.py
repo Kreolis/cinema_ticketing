c30f9ce3dacfa4f63214e73dc83aa86911c0cfba
Kreolis <info@kreolis.net>
2025-01-09 22:05:27 +0200

    fix translation bug

--- code/config/settings.py
+++ code/config/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.locale.LocaleMiddleware', # translation support
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
