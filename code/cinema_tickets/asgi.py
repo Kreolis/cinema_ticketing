0a452b906139707aba62b399807a37c1bdd674d7
Kreolis <info@kreolis.net>
2025-02-07 19:39:53 +0200

    rename to make gunicorn possible

--- code/cinema_tickets/asgi.py
+++ code/cinema_tickets/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for ticketing project.
+ASGI config for cinema_tickets project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ticketing.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cinema_tickets.settings')
 
 application = get_asgi_application()
--- code/cinema_tickets/wsgi.py
+++ code/cinema_tickets/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ticketing project.
+WSGI config for cinema_tickets project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cinema_tickets.settings')
 
 application = get_wsgi_application()
