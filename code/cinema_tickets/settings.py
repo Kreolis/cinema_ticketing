cd593bb17bfcf60dbed488352b80a518345d1ca4
Kreolis <info@kreolis.net>
2025-02-15 17:42:44 +0200

    added ALLOWED_HOSTS config

--- code/cinema_tickets/settings.py
+++ code/cinema_tickets/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DJANGO_DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+if config('ALLOWED_HOSTS', default=None):
+    ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
 
 
 # Application definition
