45db9b102bed9a4acc9184fb55bd9a311e38cdf6
Kreolis <info@kreolis.net>
2025-02-26 21:42:18 +0200

    rearranged commands

--- code/accounting/management/commands/delete_timeout_orders.py
+++ code/accounting/management/commands/delete_timeout_orders.py
@@ -1,11 +1,12 @@
 from django.core.management.base import BaseCommand
 from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+
 from accounting.models import Order
-from datetime import timedelta
 from payments.models import PaymentStatus
 
 class Command(BaseCommand):
-    help = 'Delete orders that have timed out'
+    help = _('Delete orders that have timed out')
 
     def handle(self, *args, **kwargs):
         now = timezone.now()
