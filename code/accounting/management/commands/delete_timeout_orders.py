fc441772567c6bc51e7a5648fd578f95d821bd42
Kreolis <info@kreolis.net>
2025-02-26 23:03:13 +0200

    commands tested

--- code/accounting/management/commands/delete_timeout_orders.py
+++ code/accounting/management/commands/delete_timeout_orders.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 from django.utils import timezone
+from datetime import timedelta
 from django.utils.translation import gettext_lazy as _
 
 from accounting.models import Order
@@ -10,10 +11,13 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         now = timezone.now()
-        timed_out_orders = Order.objects.filter(
-            status=PaymentStatus.WAITING,
-            has_timed_out=True,
+        waiting_orders = Order.objects.filter(
+            status=PaymentStatus.WAITING
         ) 
+        timed_out_orders = waiting_orders.filter(
+            created__lt=now - timedelta(minutes=waiting_orders.first().timeout)
+        )
+
         count = timed_out_orders.count()
         timed_out_orders.delete()
         self.stdout.write(self.style.SUCCESS(f'Successfully deleted {count} timed-out orders'))
--- code/events/management/commands/send_statistics_as_mail.py
+++ code/events/management/commands/send_statistics_as_mail.py
@@ -2,9 +2,11 @@ from django.core.management.base import BaseCommand
 from django.core.mail import EmailMessage
 from django.conf import settings
 from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
 
 from branding.models import get_active_branding
 from events.views import generate_global_statistics_pdf
+
 
 class Command(BaseCommand):
     help = _('Generate and send global event statistics to mail')
@@ -22,11 +24,20 @@ class Command(BaseCommand):
         if not branding.ticket_statistics_email:
             self.stdout.write(self.style.ERROR('No email address for ticket statistics found'))
             return
+        
+        if branding:
+            site_name = branding.site_name
+        else:
+            site_name = "Cinema Ticketing"
 
         pdf = generate_global_statistics_pdf()
+        # Create a BytesIO stream to hold the PDF data
+        pdf_output = pdf.output(dest='S')  # Get PDF as a bytearray
 
-        subject = _('Global Event Statistics')
-        message = _('Attached you will find the global event statistics for the last {interval} hours.').format(
+        subject = _('All Events Statistics - {site_name}').format(
+            site_name=site_name
+        )
+        message = _('Attached you will find the all event statistics for the last {interval} hours.').format(
             interval=branding.ticket_statistics_interval
         )
         
@@ -37,7 +48,9 @@ class Command(BaseCommand):
                 settings.DEFAULT_FROM_EMAIL,
                 [branding.ticket_statistics_email],
             )
-        email.attach('global_event_statistics.pdf', pdf, 'application/pdf')
+        
+        now = timezone.now().strftime('%Y-%m-%d_%H-%M-%S')
+        email.attach(f'all_event_statistics_{site_name}_{now}.pdf', pdf_output, 'application/pdf')
 
         try:
             email.send()
