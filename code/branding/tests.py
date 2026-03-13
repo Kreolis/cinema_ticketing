6ba5f2c61643f68793b70b68a2a7ab121c638ab0
Kreolis <info@kreolis.net>
2026-03-13 13:13:03 +0200

    correct indentations

--- code/branding/tests.py
+++ code/branding/tests.py
@@ -6,32 +6,32 @@ from branding.models import Branding, clear_active_branding_cache, get_active_br
 
 
 class ActiveBrandingCacheTests(TestCase):
-	def setUp(self):
-		clear_active_branding_cache()
+    def setUp(self):
+        clear_active_branding_cache()
 
-	def tearDown(self):
-		clear_active_branding_cache()
+    def tearDown(self):
+        clear_active_branding_cache()
 
-	def test_get_active_branding_uses_cache_after_first_lookup(self):
-		branding = Branding.objects.create(name='Active Branding', is_active=True)
+    def test_get_active_branding_uses_cache_after_first_lookup(self):
+        branding = Branding.objects.create(name='Active Branding', is_active=True)
 
-		with CaptureQueriesContext(connection) as first_lookup:
-			first_result = get_active_branding()
+        with CaptureQueriesContext(connection) as first_lookup:
+            first_result = get_active_branding()
 
-		with CaptureQueriesContext(connection) as second_lookup:
-			second_result = get_active_branding()
+        with CaptureQueriesContext(connection) as second_lookup:
+            second_result = get_active_branding()
 
-		self.assertEqual(first_result.pk, branding.pk)
-		self.assertEqual(second_result.pk, branding.pk)
-		self.assertGreater(len(first_lookup), 0)
-		self.assertEqual(len(second_lookup), 0)
+        self.assertEqual(first_result.pk, branding.pk)
+        self.assertEqual(second_result.pk, branding.pk)
+        self.assertGreater(len(first_lookup), 0)
+        self.assertEqual(len(second_lookup), 0)
 
-	def test_get_active_branding_cache_is_invalidated_when_active_branding_changes(self):
-		first_branding = Branding.objects.create(name='First Branding', is_active=True)
-		self.assertEqual(get_active_branding().pk, first_branding.pk)
+    def test_get_active_branding_cache_is_invalidated_when_active_branding_changes(self):
+        first_branding = Branding.objects.create(name='First Branding', is_active=True)
+        self.assertEqual(get_active_branding().pk, first_branding.pk)
 
-		second_branding = Branding.objects.create(name='Second Branding', is_active=True)
+        second_branding = Branding.objects.create(name='Second Branding', is_active=True)
 
-		active_branding = get_active_branding()
-		self.assertEqual(active_branding.pk, second_branding.pk)
-		self.assertFalse(Branding.objects.get(pk=first_branding.pk).is_active)
+        active_branding = get_active_branding()
+        self.assertEqual(active_branding.pk, second_branding.pk)
+        self.assertFalse(Branding.objects.get(pk=first_branding.pk).is_active)
